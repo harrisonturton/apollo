a9746ea2375d9dbc9ad40ce1ed968be6d63e7373
Harrison Turton <harrisonturton@gmail.com>
2018-07-15 13:57:06 +1000

    Parsing to a request type

--- server.hs
+++ server.hs
@@ -4,6 +4,13 @@ import System.IO
 import Network
 import Control.Concurrent
 
+data Request =
+	Request {
+    reqType :: String,
+		path    :: String,
+		options :: [String]
+  } deriving Show
+
 main = withSocketsDo $ do
   sock <- listenOn $ PortNumber 3000
   putStrLn "Listening on port 3000..."
@@ -16,3 +23,12 @@ handleAccept handle hostname = do
   putStrLn $ "Handling request from " ++ hostname
   reqLines <- hGetContents handle
   putStrLn $ show reqLines
+  putStrLn $ (show . parseRequest) reqLines
+
+parseRequest :: String -> Request
+parseRequest reqLines =
+	Request {
+    reqType = (head . words . head . lines) reqLines,
+		path    = ((!! 1) . words . head . lines) reqLines,
+		options = (tail . lines) reqLines
+  }
