cd1e45fca80609610de05b05130afcb5b1ddd347
Harrison Turton <harrisonturton@gmail.com>
2018-07-15 14:23:36 +1000

    Moved from Handles to Sockets

--- server.hs
+++ server.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad
-import System.IO
-import Network
+--import Network (withSocketsDo, listenOn, PortID(..))
+--import Network.Socket (Socket, accept)
+--import Network.Socket.ByteString (sendAll, send, recv)
+--import Control.Concurrent (forkIO)
+--import Control.Monad (forever)
+
+import Network hiding (accept)
+import Network.Socket hiding (send, recv)
+import Network.Socket.ByteString hiding (putStrLn)
+import Network.Socket.ByteString (sendAll)
 import Control.Concurrent
+import Control.Monad (forever)
 
 data Request =
 	Request {
@@ -15,15 +24,16 @@ main = withSocketsDo $ do
   sock <- listenOn $ PortNumber 3000
   putStrLn "Listening on port 3000..."
   forever $ do
-    (handle, hostname, _) <- accept sock
-    handleAccept handle hostname
+    (conn, _) <- accept sock
+    forkIO $ handleAccept conn
 
-handleAccept :: Handle -> String -> IO ()
-handleAccept handle hostname = do
-  putStrLn $ "Handling request from " ++ hostname
-  reqLines <- hGetContents handle
-  putStrLn $ show reqLines
-  putStrLn $ (show . parseRequest) reqLines
+handleAccept :: Socket -> IO ()
+handleAccept sock = do
+  putStrLn $ "Connected!"
+  rawReq <- recv sock 1024
+  putStrLn $ show rawReq
+  send sock "Hello!\n"
+  putStrLn $ (show . parseRequest . show) rawReq
 
 parseRequest :: String -> Request
 parseRequest reqLines =
