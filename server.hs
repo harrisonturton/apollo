24f3021a0dd7037a2088c5a78844d523d3f8ec7f
Harrison Turton <harrisonturton@gmail.com>
2018-07-17 17:00:06 +1000

    Working GET monad actions & handler lookup

--- server.hs
+++ server.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
 
+import qualified Control.Monad.State as ST
+import qualified Control.Monad.Reader as R
 import Network (withSocketsDo, listenOn, PortID(..))
 import Network.Socket (Socket, accept, close)
 import Network.Socket.ByteString (sendAll, recv) 
@@ -9,27 +10,50 @@ import Data.ByteString.Char8 (unpack)
 import Request
 import Response
 
-main = withSocketsDo $ do
-  sock <- listenOn $ PortNumber 3000
-  putStrLn "Listening on port 3000..."
+type Handler = R.Reader Request String
+type RoutePattern = String
+
+data ServerState =
+  ServerState {
+		handlers :: [(String, Handler)]
+  }
+
+initState = ServerState []
+
+apollo :: PortID -> ST.State ServerState () -> IO ()
+apollo port actions = do
+  sock <- listenOn $ port
+  putStrLn $ "Running Apollo on " ++ show port ++ "..."
+  let state = ST.execState actions initState
   forever $ do
     (conn, _) <- accept sock
-    async $ handleAccept conn
+    async $ handleConn conn state
 
-handleAccept :: Socket -> IO ()
-handleAccept sock = do
-  putStrLn $ "New connection!"
-  rawReq <- recv sock 1024
+handleConn :: Socket -> ServerState -> IO ()
+handleConn sock state = do
+  putStrLn "New connection!"
+  rawReq <- recv sock 4096
+  putStrLn $ unpack rawReq
   let req = parseRawRequest $ unpack rawReq
-  putStrLn $ show req
   case req of
-    Just r  -> handleRequest sock r
-    Nothing -> do
-      putStrLn "Invalid request: failed to parse Request."
-      sendAll sock rawReq
+    Just r -> handleRequest r state
+    Nothing -> putStrLn "Invalid request: could not parse request."
 
-handleRequest :: Socket -> Request -> IO ()
-handleRequest sock req = do
-  --let response = route req router
-  sendAll sock "In handleRequest!\n" 
-  --close sock -- Must close sock otherwise data isn't sent
+get :: RoutePattern -> Handler -> ST.State ServerState ()
+get route handler = do
+  (ServerState handlers) <- ST.get
+  ST.put $ ServerState $ (route, handler):handlers
+
+handleRequest :: Request -> ServerState -> IO ()
+handleRequest req state = do
+  let p = path req 
+  putStrLn $ "Request for route " ++ p
+  let handler = lookup p (handlers state)
+  case handler of
+		Just h  -> putStrLn $ show $ R.runReader h req
+		Nothing -> putStrLn $ "No handler for route " ++ p
+
+main = apollo (PortNumber 3000) $ do
+  get "/" $ do
+    req <- R.ask
+    return "Done"
